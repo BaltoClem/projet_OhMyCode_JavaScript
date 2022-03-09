5987362b3374c9a6a64bb57330892f6e6b62ec22
Clément <dumasclementjulien@yahoo.fr>
2022-03-09 14:37:06 +0100

    Manipulation javascript ok

--- initiation/script.js
+++ initiation/script.js
@@ -1,2 +1,4 @@
 console.log("Connecté !");
-document.getElementById('titre').style.color = "green";
+const titre = document.getElementById('titre'); /* le type de variable 'const' permet de ne pas changer la valeur initialiser, contraire de 'let' */
+titre = 2;
+console.log(titre);
