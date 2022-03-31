70cebb8fcea4a1c2241336a17adaf022b8edfe3d
Clément <dumasclementjulien@yahoo.fr>
2022-03-31 11:43:15 +0200

    Incrémentation au clic des cercles

--- initiation/script.js
+++ initiation/script.js
@@ -2,16 +2,25 @@
 
 const btnRed = document.getElementById('btn-red');
 //console.log(btnRed, "bouton rouge");
+const btnBlue = document.getElementById('btn-blue');
+//console.log(btnBlue, "bouton bleu")
 let compteur = 0;
 
 console.log(compteur, "compteur au démarrage");
 
-// Je soumets l'élément à une action
+// Je soumets le cercle ROUGE à une action
 btnRed.addEventListener('click', function(){
     //console.log("cliqué !");
     // Ici l'action
     // Incrémenter le compteur de 1
 
+    compteur = compteur + 1;
+    console.log(compteur, "compteur après incrémentation");
+})
+
+// Je soumets le cercle BLEU à une action
+btnBlue.addEventListener('click', function(){
+    console.log("cercle bleu cliqué !");
     compteur = compteur + 1;
     console.log(compteur, "compteur après incrémentation");
 })
