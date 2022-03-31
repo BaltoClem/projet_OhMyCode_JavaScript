bab810900071f90cc3d31e7741c49b583dc3876e
Clément <dumasclementjulien@yahoo.fr>
2022-03-31 10:47:54 +0200

    Création d'un compteur de clic

--- initiation/script.js
+++ initiation/script.js
@@ -1,4 +1,17 @@
-console.log("Connecté !");
-const titre = document.getElementById('titre'); /* le type de variable 'const' permet de ne pas changer la valeur initialiser, contraire de 'let' */
-titre = 2;
-console.log(titre);
+// Je sélectionne et je stocke
+
+const btnRed = document.getElementById('btn-red');
+//console.log(btnRed, "bouton rouge");
+let compteur = 0;
+
+console.log(compteur, "compteur au démarrage");
+
+// Je soumets l'élément à une action
+btnRed.addEventListener('click', function(){
+    //console.log("cliqué !");
+    // Ici l'action
+    // Incrémenter le compteur de 1
+
+    compteur = compteur + 1;
+    console.log(compteur, "compteur après incrémentation");
+})
