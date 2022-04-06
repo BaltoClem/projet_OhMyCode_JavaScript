94a67ac7b33df0713f9562c2879f5ccbd1c96e94
Clément <dumasclementjulien@yahoo.fr>
2022-04-06 11:29:18 +0200

    Mise en place du smiley et de l'interaction du bouton "abonnez-vous"

--- 2 - abonne/script.js
+++ 2 - abonne/script.js
@@ -1,1 +1,20 @@
-console.log("Connecté !")
+// Je sélectionne et je stocke
+
+// icone contient le premier élément <i> trouvé dans le code
+const icone = document.querySelector('i');
+console.log(icone);
+
+// btnButton contient le premier élément avec la classe btn
+const btnButton = document.querySelector('.btn')
+console.log(btnButton);
+
+icone.addEventListener('click', function(){
+    console.log('icône cliquée !');
+    icone.classList.toggle('fa-smile-wink');
+    icone.classList.toggle('happy')
+});
+
+btnButton.addEventListener('click', function(){
+    console.log('Bouton cliqué');
+    btnButton.innerText = "Abonné !";
+});
