1b4e85bbb7910da6482a3de9d861768f4bd8f2b6
Clément <dumasclementjulien@yahoo.fr>
2022-04-07 12:31:08 +0200

    Stockage des éléments

--- 3 - darkMode/script.js
+++ 3 - darkMode/script.js
@@ -1,1 +1,23 @@
-console.log("Connecté !")
+console.log("Connecté !");
+
+// Je sélectionne et je stocke
+
+// La DIV Switch
+const switchBox = document.querySelector('.switch');
+console.log(switchBox);
+
+// La DIV btn (cercle)
+const btn = document.querySelector('.btn');
+console.log(btn);
+
+// L'icône
+const icone = document.querySelector('i');
+console.log(icone);
+
+// La DIV container
+const container = document.querySelector('.container');
+console.log(container);
+
+// Le titre
+const titre = document.querySelector('h1');
+console.log(titre);
