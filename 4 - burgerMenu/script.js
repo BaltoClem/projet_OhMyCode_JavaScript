572ea0b581d46120b220cd6650c0d47504f8e615
Clément <dumasclementjulien@yahoo.fr>
2022-04-08 11:14:01 +0200

    Sélections d'éléments et actions à faire

--- 4 - burgerMenu/script.js
+++ 4 - burgerMenu/script.js
@@ -1,1 +1,18 @@
-console.log("Connecté !")
+console.log("Connecté !");
+
+// Je sélectionne et je stocke
+
+// Icône burger
+const icone = document.querySelector('.navbar-mobile i');
+console.log(icone);
+
+// DIV modal
+const modal = document.querySelector('.modal');
+console.log(modal);
+
+// Soumettre l'icône à une action
+icone.addEventListener('click', function(){
+    console.log('icône cliquée');
+    modal.classList.toggle('change-modal');
+    icone.classList.toggle('fa-times');
+});
