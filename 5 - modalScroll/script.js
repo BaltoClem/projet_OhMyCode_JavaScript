d40ca6df590752b4165fac4d8c8c8e1b44eaf875
Clément <dumasclementjulien@yahoo.fr>
2022-07-26 14:13:39 +0200

    Finalisation projet 5

--- 5 - modalScroll/script.js
+++ 5 - modalScroll/script.js
@@ -1,1 +1,38 @@
-console.log("Connecté !")
+// Je sélectionne et je stocke la DIV modal-container
+const modal = document.querySelector('.modal-container');
+console.log(modal);
+
+//Je sélectionne le bouton
+const btn = document.querySelector('.btn-success');
+console.log(btn);
+// Je sélectionne le code
+const code = document.querySelector('.code');
+console.log(code);
+// Je sélectionne l'icône
+const icone = document.querySelector('.fa-times');
+console.log(icone);
+
+window.addEventListener('scroll', displayModal);
+
+function displayModal(){
+    let hauteur = document.documentElement.scrollTop;
+    console.log('défilement détecté !');
+    if(hauteur > 400){
+        console.log('Affichage de la modale !');
+        modal.style.display = "flex";
+    };
+};
+
+btn.addEventListener('click', function() {
+    console.log('bouton cliqué');
+    code.style.display = "block";
+    btn.style.display = "none";
+});
+
+icone.addEventListener('click', function(){
+    console.log('icône cliquée');
+    // Je rends invisible la DIV modal-container
+    modal.style.display = "none";
+    // J'annule l'événement
+    window.removeEventListener('scroll', displayModal);
+})
